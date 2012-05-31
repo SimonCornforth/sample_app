2e4b610dcb604395304b4d4d985afcf46c1661df
SimonCornforth <simoncornforth@hotmail.com>
2012-05-31 19:52:28 +0100

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,13 @@ module SessionsHelper
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
+
   def sign_out
     current_user = nil
     cookies.delete(:remember_token)
@@ -34,4 +41,6 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.fullpath
   end
+
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -86,8 +86,21 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
 
-      end
-    end
+      end # "in the Users controller"
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end #"in the Microposts controller"
+    end #"for non-signed-in users"
 
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
